0d9f12bf45dc9eb5e176fb4037df10809dcffe26
Cypresskids5 <134014403+Cypresskids5@users.noreply.github.com>
2023-05-22 17:42:25 -0500

    Add files via upload

---
+++ lists.js
@@ -0,0 +1,18 @@
+export let dimensions = [
+    "./448612-Earth-space.jpg",
+    "./multiverse-images/pancake-world.png",
+    "./multiverse-images/Fornite.png",
+    "./multiverse-images/minecraft.png",
+    "./multiverse-images/snake-green-png-14.png",
+    "./multiverse-images/Demon.webp",
+    "./multiverse-images/earth.png"
+]
+
+export let dimensionPriceList = [
+    10000,
+    100000,
+    1000000,
+    10000000,
+    100000000,
+    1000000000,
+]
